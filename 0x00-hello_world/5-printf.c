4e670159046d73438edc63bf5e2a24ab3679dd59
melodyakoto <akotomeleny19@gmail.com>
2022-12-08 10:39:38 -0800

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - write a c code using printf
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}

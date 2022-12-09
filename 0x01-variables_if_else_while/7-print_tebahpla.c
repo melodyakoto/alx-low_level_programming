73571208a89371c6802e91f41b4c60d4f08d403f
melodyakoto <akotomeleny19@gmail.com>
2022-12-09 14:16:30 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main entry of a program
+ * Return: return 0 (zero)
+ */
+int main(void)
+{
+	int x = 25;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+	while (x >= 0)
+	{
+
+		putchar(alphabet[x]);
+		x--;
+	}
+	putchar('\n');
+	return (0);
+}

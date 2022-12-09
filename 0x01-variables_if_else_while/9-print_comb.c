660137c0ea9644ba4951f6c631c1099b315d18ea
melodyakoto <akotomeleny19@gmail.com>
2022-12-09 14:31:34 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main entry of a program
+ * Return: return 0 (zero)
+ */
+int main(void)
+{
+	int x = 48;
+	/* char alphabet[] = "0123456789";*/
+	while (x < 58)
+	{
+		putchar(x);
+		if (x != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		x++;
+	}
+	/* putchar('$');*/
+	putchar('\n');
+	return (0);
+}
